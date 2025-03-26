f4eb5bb9bcb3d71c15bace679edf5b56dadd8f18
unknown <marrit>
2025-03-26 14:41:27 +0100

    af

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,17 @@
 # Opdracht 1 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Marrit Kuperus
+# Groep: IT2B
 
 # Hier komt je code...
+Joe = 21
+Bob = 16
+Fred = 19
+totale_leeftijd = sum([Joe, Bob, Fred])
+aantal_personen = 3
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =" , totale_leeftijd)
+
+gemiddelde_leeftijd = totale_leeftijd / aantal_personen
+afgerond = round(gemiddelde_leeftijd, 1)
+print("De gemiddelde leeftijd van deze boys is:" , afgerond)
+
+
