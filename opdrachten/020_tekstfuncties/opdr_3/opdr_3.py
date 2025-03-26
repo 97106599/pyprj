f643d3fec2127786b069eb52eb7ec32f00a561f6
unknown <marrit>
2025-03-26 14:32:32 +0100

    af

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,20 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
-# Groep:
+# Naam student: Marrit Kuperus
+# Groep: IT2B
 
 # Hier komt je code...
+
+boom = [
+"    *      ",
+"   ***     ",
+"  ******   ",
+" ********  ",
+"***********",
+"    ***    ",
+"    ***    ",
+"    ***    ",
+]
+for line in boom:
+    print (line * 5)
+
+
