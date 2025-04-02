5b7af0d25a1a0f7610470f9d39dab5410bfaa0d3
unknown <marrit>
2025-04-02 14:34:40 +0200

    af

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -1,7 +1,14 @@
 # Opdracht 2 berekeningen
-# Naam student:
-# Groep:
-
+# Naam student: Marrit Kuperus
+# Groep: IT2B
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+gasten = ["Jij", "Paul", "Kees", "Marie", "Hilda"]
+print (gasten)
+
+gasten.remove("Marie")
+print(gasten)
+
+index_kees = gasten.index("Kees")  # Vind de positie van Kees
+gasten.insert(index_kees + 1, "George")  # Voeg George toe na Kees
+print(gasten)
