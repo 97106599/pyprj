4bd66406fd5552469c76fe467e181d3098e6d715
unknown <marrit>
2025-04-02 15:07:14 +0200

    af

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -1,9 +1,23 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Marrit Kuperus
+# Groep: IT2B
 
-# Hier komt je code...
+pizza_lijst = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
 
-# Hier start de for-loop
+pizza_lijst.sort()
+print(pizza_lijst)
 
-my_list = []
+pizza_lijst.append('yo-favorito')
+print(pizza_lijst)
+
+pizza_lijst.remove('olivio')
+print(pizza_lijst)
+
+print(pizza_lijst[:3])
+
+midden_index = len(pizza_lijst) // 2
+print([pizza_lijst[midden_index]])
+
+print(pizza_lijst[-3:])
+
+
